c1f4e205582d86861119212e2507f9997db3f948
SANJAYBONAGIRI <sanjay.bonagiri@accenture.com>
2025-02-27 03:13:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+  return {
+    member: {
+      name: 'John Doe',
+      skills: {
+        frontEnd: {
+          html: true,
+          css: true,
+          js: true,
+        },
+        backEnd: {
+          node: true,
+          php: false,
+          python: false,
+        },
+      },
+    },
+  };
+}
